22c3aa86786abe5c6b8f009249d868fc43717c80
Pritisha Singh <pritisha404@gmail.com>
2026-04-05 22:34:33 +0530

    Time: 3 ms (99.62%) | Memory: 46.1 MB (42.38%) - LeetSync

--- 657-robot-return-to-origin/robot-return-to-origin.java
+++ 657-robot-return-to-origin/robot-return-to-origin.java
@@ -1,21 +1,32 @@
+// class Solution {
+//     public boolean judgeCircle(String moves) {
+//        int x=0;
+//        int y=0;
+//         for(int i=0; i<moves.length(); i++ ){
+//             if(moves.charAt(i)=='U'){
+//                 y++;
+//             } 
+//             else if(moves.charAt(i)=='D'){
+//                 y--;
+//             } 
+//             else if(moves.charAt(i)=='R'){
+//                 x++;
+//             } 
+//             else if(moves.charAt(i)=='L'){
+//                 x--;
+//             }
+//         }
+//         return (x==0 && y==0);
+//     }
+// }
+
 class Solution {
     public boolean judgeCircle(String moves) {
-       int x=0;
-       int y=0;
-        for(int i=0; i<moves.length(); i++ ){
-            if(moves.charAt(i)=='U'){
-                y++;
-            } 
-            else if(moves.charAt(i)=='D'){
-                y--;
-            } 
-            else if(moves.charAt(i)=='R'){
-                x++;
-            } 
-            else if(moves.charAt(i)=='L'){
-                x--;
-            }
+        int[] ch = new int[26];
+        for (char move : moves.toCharArray()){
+            ch[move - 'A']++;
         }
-        return (x==0 && y==0);
+        return ch['U' - 'A'] == ch['D' - 'A'] && 
+                ch['L' - 'A'] == ch['R' - 'A'];
     }
 }
