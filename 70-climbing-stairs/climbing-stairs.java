14db4dacfb7e0369a982852cabe12c37c842b142
Pritisha Singh <pritisha404@gmail.com>
2026-04-14 23:34:17 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (7.72%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,18 +1,35 @@
+// class Solution {
+//     public int climbStairs(int n) {
+//         int dp[]=new int[n+1];
+//         return fib(n, dp);
+//     }
+
+//     public int fib(int n, int dp[]){
+//         if(n<0) return 0;
+//         if(n==0 || n==1) {
+//            return 1;
+
+//         }
+//         if(dp[n]!=0) return dp[n];
+//         dp[n]=fib(n-1,dp)+fib(n-2,dp);
+
+//         return dp[n];
+//     }
+// }
+
 class Solution {
+    //tabulation
     public int climbStairs(int n) {
-        int dp[]=new int[n+1];
-        return fib(n, dp);
-    }
-
-    public int fib(int n, int dp[]){
         if(n<0) return 0;
-        if(n==0 || n==1) {
-           return 1;
-
+        if(n==0 || n==1) return 1;
+        int dp[]=new int[n+1];
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2; i<=n; i++){
+            dp[i]=dp[i-1]+dp[i-2];
         }
-        if(dp[n]!=0) return dp[n];
-        dp[n]=fib(n-1,dp)+fib(n-2,dp);
-
         return dp[n];
     }
+
+   
 }
