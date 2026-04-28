29e3d8c1dd4f31e225f19523bd7aaa8af3f4169f
Pritisha Singh <pritisha404@gmail.com>
2026-04-28 23:34:17 +0530

    Time: 3 ms (25.20%) | Memory: 43.2 MB (90.23%) - LeetSync

---
+++ 10-regular-expression-matching/regular-expression-matching.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+class Solution {
+    public boolean isMatch(String s, String p) {
+        int n = s.length();
+        int m = p.length();
+
+        boolean[][] dp = new boolean[n + 1][m + 1];
+        dp[0][0] = true;
+
+    
+        for (int j = 1; j <= m; j++) {
+            if (p.charAt(j - 1) == '*') {
+                dp[0][j] = dp[0][j - 2];
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+
+                // Case 1: direct match or '.'
+                if (p.charAt(j - 1) == s.charAt(i - 1) || p.charAt(j - 1) == '.') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+
+                // Case 2: '*'
+                else if (p.charAt(j - 1) == '*') {
+
+                    // 0 occurrence
+                    dp[i][j] = dp[i][j - 2];
+
+                    // 1 or more occurrence
+                    if (p.charAt(j - 2) == s.charAt(i - 1) || p.charAt(j - 2) == '.') {
+                        dp[i][j] |= dp[i - 1][j];
+                    }
+                }
+
+                else {
+                    dp[i][j] = false;
+                }
+            }
+        }
+
+        return dp[n][m];
+    }
+}
