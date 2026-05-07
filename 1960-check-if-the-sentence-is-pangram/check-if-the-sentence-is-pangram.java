fce50d2b01706429f2903924726fad2433110113
Pritisha Singh <pritisha404@gmail.com>
2026-05-07 20:59:24 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (92.62%) - LeetSync

--- 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
@@ -1,14 +1,21 @@
 class Solution {
     public boolean checkIfPangram(String sentence) {
-        boolean flag[]=new boolean[26];
-        for(int i=0; i<sentence.length(); i++){
-            if(flag[sentence.charAt(i)-'a']) continue;
-            else flag[sentence.charAt(i)-'a']=true;
-        }
+        // boolean flag[]=new boolean[26];
+        // for(int i=0; i<sentence.length(); i++){
+        //     if(flag[sentence.charAt(i)-'a']) continue;
+        //     else flag[sentence.charAt(i)-'a']=true;
+        // }
 
-        for(int i=0; i<26; i++){
-            if(!flag[i]) return false;
+        // for(int i=0; i<26; i++){
+        //     if(!flag[i]) return false;
+        // }
+        // return true;
+          if(sentence.length()<26) return false;
+        for(char ch ='a';ch<='z';ch++){
+            if(sentence.indexOf(ch)<0){
+                return false;
+            }
         }
-        return true;
+        return true;    
     }
 }
