b139630d4dacade2c0c0b8e1db7f5c8e0c918f31
Pritisha Singh <pritisha404@gmail.com>
2026-05-19 22:03:22 +0530

    Time: 2 ms (88.98%) | Memory: 80.3 MB (63.48%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.java
+++ 2634-minimum-common-value/minimum-common-value.java
@@ -1,15 +1,33 @@
+// class Solution {
+//     public int getCommon(int[] nums1, int[] nums2) {
+//         HashSet<Integer> hs=new HashSet<>();
+//         for(int i=0; i<nums1.length; i++){
+//             hs.add(nums1[i]);
+//         }
+
+//         for(int i=0; i<nums2.length; i++){
+//             if(hs.contains(nums2[i])){
+//                 return nums2[i];
+//             }
+//         }
+//         return -1;
+//     }
+// }
+
 class Solution {
     public int getCommon(int[] nums1, int[] nums2) {
-        HashSet<Integer> hs=new HashSet<>();
-        for(int i=0; i<nums1.length; i++){
-            hs.add(nums1[i]);
-        }
-
-        for(int i=0; i<nums2.length; i++){
-            if(hs.contains(nums2[i])){
-                return nums2[i];
+        int nums1Pointer = 0;
+        int nums2Pointer = 0;
+        while (nums1Pointer < nums1.length && nums2Pointer < nums2.length) {
+            if (nums1[nums1Pointer] == nums2[nums2Pointer]) {
+                return nums1[nums1Pointer];
+            } else if (nums1[nums1Pointer] > nums2[nums2Pointer]) {
+                nums2Pointer++;
+            } else {
+                nums1Pointer++;
             }
         }
+
         return -1;
     }
 }
