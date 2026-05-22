fdeacd7b09d6383fac2ebbdb251dea8289106a42
Pritisha Singh <pritisha404@gmail.com>
2026-05-23 00:35:04 +0530

    Time: 1 ms (100.00%) | Memory: 46.5 MB (67.31%) - LeetSync

--- 445-add-two-numbers-ii/add-two-numbers-ii.java
+++ 445-add-two-numbers-ii/add-two-numbers-ii.java
@@ -1,3 +1,53 @@
+// /**
+//  * Definition for singly-linked list.
+//  * public class ListNode {
+//  *     int val;
+//  *     ListNode next;
+//  *     ListNode() {}
+//  *     ListNode(int val) { this.val = val; }
+//  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+//  * }
+//  */
+// class Solution {
+//     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+//         l1=reverse(l1);
+//         l2=reverse(l2);
+
+        
+//         int carry=0;
+//         ListNode ans=null;
+//         while(l1!=null || l2!=null || carry!=0){
+//             int sum=carry;
+//             if(l1!=null){
+//                 sum+=l1.val;
+//                 l1=l1.next;
+//             }
+//             if(l2!=null){
+//                 sum+=l2.val;
+//                 l2=l2.next;
+//             }
+//             int digit=sum%10;
+//             carry=sum/10;            
+//             ListNode newNode=new ListNode(digit);
+//             newNode.next=ans;
+//             ans=newNode;
+//         }  
+
+//         return ans;
+//     }
+
+//     public ListNode reverse(ListNode head){
+//         if(head==null || head.next==null){
+//             return head;
+//         }
+//         ListNode last=reverse(head.next);
+//         head.next.next=head;
+//         head.next=null;
+//         return last;
+//     }
+
+
+// }
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -9,42 +59,58 @@
  * }
  */
 class Solution {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        l1=reverse(l1);
-        l2=reverse(l2);
 
-        
-        int carry=0;
-        ListNode ans=null;
-        while(l1!=null || l2!=null || carry!=0){
-            int sum=carry;
-            if(l1!=null){
-                sum+=l1.val;
-                l1=l1.next;
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        l1 = reverse(l1);
+        l2 = reverse(l2);
+
+        int carry = 0;
+        ListNode ans = new ListNode(0);
+
+        while (l1 != null || l2 != null) {
+
+            int sum = carry;
+
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
             }
-            if(l2!=null){
-                sum+=l2.val;
-                l2=l2.next;
+
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
             }
-            int digit=sum%10;
-            carry=sum/10;            
-            ListNode newNode=new ListNode(digit);
-            newNode.next=ans;
-            ans=newNode;
-        }  
+
+            carry = sum / 10;
+            sum = sum % 10;
+
+            ans.val = sum;
+
+            ListNode newNode = new ListNode(carry);
+            newNode.next = ans;
+            ans = newNode;
+        }
+
+        // If no carry, remove extra node
+        if (ans.val == 0) {
+            return ans.next;
+        }
 
         return ans;
     }
 
-    public ListNode reverse(ListNode head){
-        if(head==null || head.next==null){
+    public ListNode reverse(ListNode head) {
+
+        if (head == null || head.next == null) {
             return head;
         }
-        ListNode last=reverse(head.next);
-        head.next.next=head;
-        head.next=null;
+
+        ListNode last = reverse(head.next);
+
+        head.next.next = head;
+        head.next = null;
+
         return last;
     }
-
-
 }
