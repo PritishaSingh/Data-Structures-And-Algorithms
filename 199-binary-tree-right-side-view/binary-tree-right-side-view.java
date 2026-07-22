96345f7cc28c59c346dc1389149e7a3ab67e7e30
Pritisha Singh <pritisha404@gmail.com>
2026-07-22 22:19:17 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (32.07%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -15,21 +15,36 @@
  */
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
-        List<Integer> ans=new ArrayList<>();
-        Queue<TreeNode> q=new LinkedList<>();
-        if(root==null) return ans;
-        q.add(root);
-        while(!q.isEmpty()){
-            int size=q.size();
-            for(int i=0; i<size; i++){
-                TreeNode curr=q.remove();
-                if(i==size-1) ans.add(curr.val);
-                if(curr.left!=null) q.add(curr.left);
-                if(curr.right!=null) q.add(curr.right);
+        //Breadth First Search Code
+        // List<Integer> ans=new ArrayList<>();
+        // Queue<TreeNode> q=new LinkedList<>();
+        // if(root==null) return ans;
+        // q.add(root);
+        // while(!q.isEmpty()){
+        //     int size=q.size();
+        //     for(int i=0; i<size; i++){
+        //         TreeNode curr=q.remove();
+        //         if(i==size-1) ans.add(curr.val);
+        //         if(curr.left!=null) q.add(curr.left);
+        //         if(curr.right!=null) q.add(curr.right);
                 
-            }
+        //     }
 
-        }
-        return ans;
+        // }
+        // return ans;
+
+        // DFS code
+
+        List<Integer> ans=new ArrayList<>();
+        int level=1;
+         preorder(root, ans, level);
+         return ans;
+    }
+
+    public void preorder(TreeNode root, List<Integer> ans, int level){
+        if(root==null) return;
+        if(level>ans.size()) ans.add(root.val);
+        preorder(root.right, ans, level+1);
+        preorder(root.left, ans, level+1);
     }
 }
