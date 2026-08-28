66b8617702c8f02f6d2ab9fa4b6870ada43b761c
Pritisha Singh <pritisha404@gmail.com>
2026-08-28 22:57:47 +0530

    Time: 1 ms (100.00%) | Memory: 42.8 MB (91.44%) - LeetSync

--- 1298-reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.java
+++ 1298-reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.java
@@ -1,34 +1,62 @@
 class Solution {
     public String reverseParentheses(String s) {
-        Stack<Integer> st = new Stack<>();
-        StringBuilder res = new StringBuilder();
+        // Stack<Integer> st = new Stack<>();
+        // StringBuilder res = new StringBuilder();
 
-        for (int i = 0; i < s.length(); i++) {
+        // for (int i = 0; i < s.length(); i++) {
 
-            if (s.charAt(i) == '(') {
-                st.push(res.length());
-            } 
-            else if (s.charAt(i) == ')') {
-                int j = st.pop();
+        //     if (s.charAt(i) == '(') {
+        //         st.push(res.length());
+        //     } 
+        //     else if (s.charAt(i) == ')') {
+        //         int j = st.pop();
 
-                // Reverse characters between '(' and ')'
-                int left = j;
-                int right = res.length() - 1;
+        //         // Reverse characters between '(' and ')'
+        //         int left = j;
+        //         int right = res.length() - 1;
 
-                while (left < right) {
-                    char temp = res.charAt(left);
-                    res.setCharAt(left, res.charAt(right));
-                    res.setCharAt(right, temp);
+        //         while (left < right) {
+        //             char temp = res.charAt(left);
+        //             res.setCharAt(left, res.charAt(right));
+        //             res.setCharAt(right, temp);
 
-                    left++;
-                    right--;
-                }
-            } 
-            else {
+        //             left++;
+        //             right--;
+        //         }
+        //     } 
+        //     else {
+        //         res.append(s.charAt(i));
+        //     }
+        // }
+
+        // return res.toString();
+
+
+        //APPROACH 2
+        int n=s.length();
+        Stack<Integer> opi=new Stack<>();
+        int door[]=new int[n];
+        for(int i=0; i<n; i++){
+            if(s.charAt(i)=='('){
+                opi.push(i);
+            }else if(s.charAt(i)==')'){
+                int j=opi.pop();
+                door[i]=j;
+                door[j]=i;
+
+            }
+        }
+        StringBuilder res=new StringBuilder();
+        int flag=1;
+        for(int i=0; i<n; i+=flag){
+            if(s.charAt(i)=='(' || s.charAt(i)==')'){
+                i=door[i];
+                flag=-flag;
+            }else{
                 res.append(s.charAt(i));
             }
         }
+    return res.toString();
 
-        return res.toString();
     }
 }
