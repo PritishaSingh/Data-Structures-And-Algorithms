869a97b706c16b02a007a9bf8569b7ca2ff629d4
Pritisha Singh <pritisha404@gmail.com>
2026-09-03 00:12:28 +0530

    Time: 0 ms (100.00%) | Memory: 45.3 MB (35.83%) - LeetSync

---
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        return true;
+        // as all number are even 
+        // all number are odd
+        // we can change parity of an even number to odd if num is mix of even and odd numbers as even-odd=odd
+    }
+}
