fbc841d89afdd0993b437a17e3b7dbb145c8d719
Pritisha Singh <pritisha404@gmail.com>
2026-09-25 23:27:38 +0530

    Time: 1 ms (99.98%) | Memory: 45.8 MB (10.70%) - LeetSync

--- 113-path-sum-ii/path-sum-ii.java
+++ 113-path-sum-ii/path-sum-ii.java
@@ -14,25 +14,28 @@
  * }
  */
 class Solution {
-    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
-        List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> path = new ArrayList<>();
-        dfs(root, targetSum, path, ans);
-        return ans;
-    }
+    List<List<Integer>> res;
 
-    public void dfs(TreeNode node, int target, List<Integer> path, List<List<Integer>> ans){
-        if(node == null) return;
-
-        path.add(node.val);
-
-        if(node.left == null && node.right == null && target == node.val){
-            ans.add(new ArrayList<>(path));
+    void fill (TreeNode root, int sum, List<Integer> temp, int targetSum){
+        if(root==null) return;
+        sum+=root.val;
+        temp.add(root.val);
+        if(root.left==null && root.right==null){
+            if(sum==targetSum){
+                res.add(new ArrayList<>(temp));
+            }
+            temp.remove(temp.size()-1);
+            return;
         }
-
-        dfs(node.left, target - node.val, path, ans);
-        dfs(node.right, target - node.val, path, ans);
-
-        path.remove(path.size() - 1); // backtrack
+        fill(root.left, sum, temp, targetSum);
+        fill(root.right, sum, temp, targetSum);
+          temp.remove(temp.size()-1);
+    }
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        res=new ArrayList<>();
+        int s=0;
+        List<Integer> temp=new ArrayList<>();
+        fill(root, s, temp,targetSum);
+        return res;
     }
 }
